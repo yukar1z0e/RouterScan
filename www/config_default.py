8fc2f404de78a03f687e2d339d9d7517b37ec9ee
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2020-05-07 15:32:17 +0800

    test_env_config file

---
+++ www/config_default.py
@@ -0,0 +1,13 @@
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www',
+        'password': 'www',
+        'db': 'router_scan'
+    },
+    'session': {
+        'secret': 'RouterScan'
+    }
+}
