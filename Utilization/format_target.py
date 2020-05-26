9206aae8e6eea476f3b7bde71f66bac6e8c61e40
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2020-05-26 16:16:14 +0800

    Router Scan Update

--- Utilization/format_target.py
+++ Utilization/format_target.py
@@ -3,8 +3,14 @@ import ipcalc
 
 class Format:
     def __init__(self):
+        self.target_list = []
+        self.result = []
+        self.port = 443
+
+        # Old Version of Param Never Use(Only For CVE_2018_13379)
         self.default_port = 443
 
+    # Old Version of Function Never Use(Only For CVE_2018_13379)
     def format_file(self, filename):
         target_info = []
         for line in open(filename, 'r'):
@@ -24,6 +30,7 @@ class Format:
                     target_info.append(ip)
         return target_info
 
+    # Old Version of Function Never Use(Only For CVE_2018_13379)
     def format_ip(self, target):
         target_info = []
         target_check_list = target.split('/')
@@ -41,6 +48,50 @@ class Format:
                 ip = [str(i), self.default_port]
                 target_info.append(ip)
         return target_info
+
+    # New Version Function
+    def ImportTargetFromFile(self, filename):
+        with open(filename) as file:
+            self.target_list = file.read().splitlines()
+
+    # New Version Function
+    def ImportTargetFromIp(self, ip):
+        if not self.target_list:
+            self.target_list.append(ip)
+
+    # New Version Function
+    def FormatTarget(self, target):
+        ret = []
+        # 192.168.1.1 192.168.1.1:80 192.168.1.1/16
+        if len(target.split(':')) == 2:
+            # 192.168.1.1:80
+            ip = target.split(':')[0]
+            port = target.split(':')[1]
+            ret.append([ip, port])
+        elif len(target.split('/')) == 2:
+            # 192.168.1.1/16
+            ip_list = ipcalc.Network(target)
+            for ip in ip_list:
+                ret.append([str(ip), self.port])
+        else:
+            # 192.168.1.1
+            ret.append([target, self.port])
+        return ret
+
+    # New Version Function
+    def GetTarget(self):
+        self.result = []
+        for target in self.target_list:
+            self.result.extend(self.FormatTarget(target))
+        ret = self.result
+        return ret
+
+    # New Version Function
+    def __del__(self):
+        # print('del class')
+        self.target_list = []
+        self.result = []
+        self.port = 443
 
 
 if __name__ == '__main__':
